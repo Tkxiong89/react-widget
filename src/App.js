e975f45a5fd6c55ae8defe3cf067dcc54be48146
Tkxiong89 <txiong401@gmail.com>
2022-02-07 21:26:22 -0600

    added tabs component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
 import Clock from './components/clock/Clock';
 import Weather from './components/weather/Weather';
+import Tabs from './components/tabs/Tabs';
 
+
+const panes = [
+  {title: 'one', content: 'I am the first'},
+  {title: 'two', content: 'Second pane here'},
+  {title: 'three', content: 'Third pane here'}
+];
 function App() {
   return (
     <div className="App">
       <Clock/>
       <Weather />
+      <div className="interactive">
+        <Tabs panes={panes}/>
+      </div>
     </div>
   );
 }
